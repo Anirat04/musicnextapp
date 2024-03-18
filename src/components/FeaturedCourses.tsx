4f359b4ddc10d315e707a972ef4760b85e451b75
Tanbir Haque <tanbirhaque53@gmail.com>
2024-03-19 05:42:30 +0600

    All course card added

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -28,7 +28,7 @@ const FeaturedCourses = () => {
                     <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Learn With the Best</p>
                 </div>
             </div>
-            <div className="mt-10 mx-8">
+            <div className="mt-10 mx-8 max-w-screen-xl mx-auto">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
                     {featuredCourses.map((course: Course) => (
                         <div key={course.id} className="flex justify-center">
