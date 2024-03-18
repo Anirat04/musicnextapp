9808690903e6e7abe80626ff1cc045031dce0b49
Tanbir Haque <tanbirhaque53@gmail.com>
2024-03-19 05:11:13 +0600

    Homepage completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -7,6 +12,11 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <HeroSection></HeroSection>
       <FeaturedCourses></FeaturedCourses>
+      <WhyChooseUs></WhyChooseUs>
+      <MusicSchoolTestimonials></MusicSchoolTestimonials>
+      <UpcomingWebinars></UpcomingWebinars>
+      <Instructors></Instructors>
+      <Footer></Footer>
     </main>
   );
 }
